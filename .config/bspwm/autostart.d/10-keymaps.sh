f2d6242d43b16e0d917713e21271d5cc9a660a62
ivanp7 <ivanp7@mail.ru>
2023-03-26 22:11:09 +0300

    refactor: change paths to configuration directories

--- .config/bspwm/autostart.d/10-keymaps.sh
+++ .config/bspwm/autostart.d/10-keymaps.sh
@@ -6,7 +6,7 @@ sleep 1
 sysmodmap="/etc/X11/xinit/.Xmodmap"
 [ ! -f "$sysmodmap" ] || xmodmap "$sysmodmap"
 
-usermodmap="${XDG_CONFIG_HOME:-"$HOME/.config"}/X11/xmodmap"
+usermodmap="${XDG_CONFIG_HOME:-"$HOME/.config"}/X11/xmodmap.d"
 [ ! -d "$usermodmap" ] ||
 for file in $(find "$usermodmap" -type f,l)
 do

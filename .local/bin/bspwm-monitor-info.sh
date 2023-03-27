8d1500968074dec979bbadcb79dbacda85f786b4
ivanp7 <ivanp7@mail.ru>
2023-03-27 18:09:22 +0300

    fix: several minor fixes

--- .local/bin/bspwm-monitor-info.sh
+++ .local/bin/bspwm-monitor-info.sh
@@ -3,7 +3,7 @@
 INFO_TYPE="$1"
 : ${SELECTOR:=${2:-primary}}
 
-MONITOR_JSON="$(bspc query -T -m "$SELECTOR")"
+MONITOR_JSON="$(bspc query -T -m $SELECTOR)"
 MONITOR_NAME="$(echo "$MONITOR_JSON" | jq --raw-output ".name")"
 MONITOR_RECTANGLE="$(echo "$MONITOR_JSON" | jq ".rectangle")"
 MONITOR_X="$(echo "$MONITOR_RECTANGLE" | jq ".x")"
@@ -12,8 +12,8 @@ MONITOR_WIDTH="$(echo "$MONITOR_RECTANGLE" | jq ".width")"
 MONITOR_HEIGHT="$(echo "$MONITOR_RECTANGLE" | jq ".height")"
 
 case "$INFO_TYPE" in
-    slop) echo "${MONITOR_WIDTH}x${MONITOR_HEIGHT}+${MONITOR_X}+${MONITOR_Y}" ;;
     name) echo "$MONITOR_NAME" ;;
+    geometry) echo "${MONITOR_WIDTH}x${MONITOR_HEIGHT}+${MONITOR_X}+${MONITOR_Y}" ;;
     area) echo "$MONITOR_WIDTH $MONITOR_HEIGHT $MONITOR_X $MONITOR_Y" ;;
     width)  echo "$MONITOR_WIDTH" ;;
     height) echo "$MONITOR_HEIGHT" ;;

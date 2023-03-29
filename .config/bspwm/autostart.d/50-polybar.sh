43c4648e7b83f6709c7fded9cddb4d103d8f7d77
ivanp7 <ivanp7@mail.ru>
2023-03-30 02:24:24 +0300

    refactor: move MONITOR_* variables out of env.shell

--- .config/bspwm/autostart.d/50-polybar.sh
+++ .config/bspwm/autostart.d/50-polybar.sh
@@ -5,6 +5,8 @@ export WINDOW_TITLE_FORMAT="%title:0:$(($(bspwm-monitor-info.sh width) / $DEFAUL
 export FONT="$DEFAULT_FONT;0"
 export PREFIX="$(cat /sys/class/tty/tty0/active):$(id -un)"
 
+MONITOR_PRIMARY="$(bspwm-monitor-info.sh name primary)"
+
 xrandr | sed '/ [0-9]\+x[0-9]\++[0-9]\++[0-9]\+ /!d; s/ .*//' | while IFS="
 " read -r monitor
 do

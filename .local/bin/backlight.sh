47422d069338aaafdd84c0f47f72e3e6b7416f80
ivanp7 <ivanp7@mail.ru>
2023-03-31 00:41:27 +0300

    feat(.local/bin/backlight.sh): control backlight using /sys/class/backlight interface

---
+++ .local/bin/backlight.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eu
+
+: ${DEVICE:="$(cd /sys/class/backlight; find . -mindepth 1 -maxdepth 1 -type d,l | head -1 | sed 's|^\./||')"}
+
+[ "$DEVICE" ] || exit 1
+
+MAX_BRIGHTNESS=$(cat "/sys/class/backlight/$DEVICE/max_brightness")
+STEP=$(($MAX_BRIGHTNESS / 10))
+BRIGHTNESS=$(cat "/sys/class/backlight/$DEVICE/brightness")
+echo "$BRIGHTNESS/$MAX_BRIGHTNESS"
+
+if [ "${1:-}" ]
+then
+    case "$1" in
+        up)   BRIGHTNESS=$(($BRIGHTNESS + $STEP)) ;;
+        down) BRIGHTNESS=$(($BRIGHTNESS - $STEP)) ;;
+        +*) BRIGHTNESS=$(($BRIGHTNESS $1)) ;;
+        -*) BRIGHTNESS=$(($BRIGHTNESS $1)) ;;
+        *) BRIGHTNESS="$1" ;;
+    esac
+
+    echo "$BRIGHTNESS" > "/sys/class/backlight/$DEVICE/brightness"
+
+    BRIGHTNESS=$(cat "/sys/class/backlight/$DEVICE/brightness")
+    echo "-> $BRIGHTNESS/$MAX_BRIGHTNESS"
+fi
+

a615140b0b69127d7bb7efbd6c576a301ff52d1a
ivanp7 <ivanp7@mail.ru>
2023-04-01 08:14:47 +0300

    fix(mpv-wrapper.sh): add forgotten script

---
+++ .local/bin/mpv-wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$1" = "--wid" ]
+then
+    WID="$2"
+    shift 2
+
+    MPV_ARGS="--force-window=yes --wid=$WID ${MPV_ARGS:-}"
+fi
+
+${VIDEO_ACCELERATOR:-} mpv ${MPV_ARGS:-} "$@"
+

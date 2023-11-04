c2dbb459d04b9b07e84e336732b6ef42a5bdd887
ivanp7 <ivanp7@mail.ru>
2023-11-04 12:37:26 +0300

    fix(bin/pass-dmenu.sh): redirect pass stdout to /dev/null

--- .local/bin/pass-dmenu.sh
+++ .local/bin/pass-dmenu.sh
@@ -11,5 +11,5 @@ export PINENTRY_USER_DATA=X
 NAME="$(find . -path "./.git" -prune -o -type f -name "*.gpg" -print | sed 's|^\./||; s|\.gpg$||' |
     dmenu.sh -p "$DMENU_PROMPT" -l "$DMENU_LINES" -g "$DMENU_COLUMNS" | head -1)"
 
-[ -z "$NAME" ] || pass -c "$NAME"
+[ -z "$NAME" ] || pass -c "$NAME" > /dev/null
 

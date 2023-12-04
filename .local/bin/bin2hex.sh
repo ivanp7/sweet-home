8e26964ae848bc0663ab5d5a317cce63bc72bde0
ivanp7 <ivanp7@mail.ru>
2023-12-04 20:51:53 +0300

    feat(bin): accept multiple arguments for hex2bin.sh and bin2hex.sh

--- .local/bin/bin2hex.sh
+++ .local/bin/bin2hex.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-BIN="$1"
-shift 1
-
+BIN="$@"
+BIN="$(echo "$BIN" | tr -d ' ')"
 echo "$BIN" | grep -q "[^01]" && exit 1
 
 echo "obase=16; ibase=2; $BIN" | bc
--- .local/bin/hex2bin.sh
+++ .local/bin/hex2bin.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-HEX="$1"
-shift 1
+HEX="$@"
+HEX="$(echo "$HEX" | tr -d ' ' | tr 'abcdef' 'ABCDEF')"
+echo "$HEX" | grep -q "[^A-F0-9]" && exit 1
 
-echo "$HEX" | grep -q "[^A-Fa-f0-9]" && exit 1
-
-HEX="$(echo "$HEX" | tr 'abcdef' 'ABCDEF')"
 echo "obase=2; ibase=16; $HEX" | bc
 

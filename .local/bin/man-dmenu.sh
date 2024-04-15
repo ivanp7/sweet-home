7cfc7141f87b09b0ef2845da6943c9da958d3f66
ivanp7 <ivanp7@mail.ru>
2024-04-15 17:41:32 +0300

    feat(bin/man-dmenu.sh): choose pages from the full available list, not history

--- .local/bin/man-dmenu.sh
+++ .local/bin/man-dmenu.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-DMENU_PROMPT="[Section] Page"
-: ${DMENU_LINES:=20}
-: ${DMENU_COLUMNS:=10}
+DMENU_PROMPT="Page"
+: ${DMENU_LINES:=30}
+: ${DMENU_COLUMNS:=2}
 
 HISTORY_FILE="$XDG_CACHE_HOME/man-history"
 touch -- "$HISTORY_FILE"
 
-INPUT=$(tac -- "$HISTORY_FILE" | uniq | dmenu.sh -p "$DMENU_PROMPT" -l "$DMENU_LINES" -g "$DMENU_COLUMNS" | head -1)
+INPUT=$(man -k "" | # tac -- "$HISTORY_FILE"
+    uniq | dmenu.sh -p "$DMENU_PROMPT" -l "$DMENU_LINES" -g "$DMENU_COLUMNS" | head -1 |
+    sed "s,^\(\S\+\) (\(\S\+\))\s\+-.*,\2 \1,")
 [ "$INPUT" ] || exit
 
 if man $INPUT > /dev/null 2>&1

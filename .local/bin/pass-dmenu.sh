6463707ff05ce6d2788e662b666c5b10ad74a40f
ivanp7 <ivanp7@mail.ru>
2024-04-16 20:13:41 +0300

    feat(bin/pass-dmenu.sh): show notification after password copy

--- .local/bin/pass-dmenu.sh
+++ .local/bin/pass-dmenu.sh
@@ -4,6 +4,8 @@ DMENU_PROMPT="Password"
 : ${DMENU_LINES:=20}
 : ${DMENU_COLUMNS:=3}
 
+: ${NOTIFY_TIME:=3000}
+
 cd -- "$PASSWORD_STORE_DIR"
 
 export PINENTRY_USER_DATA=X
@@ -11,5 +13,6 @@ export PINENTRY_USER_DATA=X
 NAME="$(find . -path "./.git" -prune -o -type f -name "*.gpg" -print | sed 's|^\./||; s|\.gpg$||' |
     dmenu.sh -p "$DMENU_PROMPT" -l "$DMENU_LINES" -g "$DMENU_COLUMNS" | head -1)"
 
-[ -z "$NAME" ] || pass -c "$NAME" > /dev/null
+[ -z "$NAME" ] || { pass -c "$NAME" > /dev/null &&
+    notify-send -t "$NOTIFY_TIME" -u normal -- "pass-dmenu.sh" "$NAME"; }
 

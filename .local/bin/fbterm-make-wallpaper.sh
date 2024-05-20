3051f325c33efc0c098b6b2674bcbbbfa18e16ba
ivanp7 <ivanp7@mail.ru>
2024-05-20 03:25:26 +0300

    fix(bin/fbterm-make-wallpaper.sh): delete interfering double quotes

--- .local/bin/fbterm-make-wallpaper.sh
+++ .local/bin/fbterm-make-wallpaper.sh
@@ -26,5 +26,5 @@ fi
 
 ( sleep "$FBI_DELAY"; cat -- "$FRAMEBUFFER" > "$OUTPUT" ) &
 fbi -noverbose -nocomments -nointeractive -autozoom -once \
-    -timeout $(("$FBI_DELAY" * 2)) -device "$FRAMEBUFFER" "$INPUT"
+    -timeout $(($FBI_DELAY * 2)) -device "$FRAMEBUFFER" "$INPUT"
 

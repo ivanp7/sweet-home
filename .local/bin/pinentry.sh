63e87c8894942c0ffd3efc66dd9ea86cbbeefae0
ivanp7 <ivanp7@mail.ru>
2025-04-04 15:22:45 +0300

    feat(bin/pinentry.sh): update pinentry binary name

--- .local/bin/pinentry.sh
+++ .local/bin/pinentry.sh
@@ -3,7 +3,7 @@
 TIMEOUT=300
 
 case "$PINENTRY_USER_DATA" in
-    X)      exec /usr/bin/pinentry-qt5    -o $TIMEOUT "$@" ;;
+    X)      exec /usr/bin/pinentry-qt6    -o $TIMEOUT "$@" ;;
     curses) exec /usr/bin/pinentry-curses -o $TIMEOUT "$@" ;;
     *)      exec /usr/bin/pinentry-tty    -o $TIMEOUT "$@" ;;
 esac
